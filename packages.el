d3b363cdfce2be27841fa0282bc16e2babb346b7
pcs155251 <pcs155251@gmail.com>
2023-01-03 19:28:11 +0800

    minor modification

--- packages.el
+++ packages.el
@@ -8,7 +8,6 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
-
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
@@ -48,3 +47,7 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! org-fragtog)
+(package! org-elp)
+(package! modus-theme)
